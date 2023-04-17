7b7d9e320aed42a87e67ffb8984dd1b814613f64
Sandeep Kushwaha <saileshkush95@gmail.com>
2023-04-17 08:49:22 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function App() {
+
+  const [isDarModeActive, setIsDarModeActive] = useState(true)
+
+  const [themeColor, setThemeColor] = useState("red")
+
+  const handleDarkModeSelection = () => {
+    // use can also use user input value
+    if (isDarModeActive) {
+      document.documentElement.classList.remove('dark')
+      setIsDarModeActive(false)
+    } else {
+      document.documentElement.classList.add('dark')
+      setIsDarModeActive(true)
+    }
+  }
+
+  const handleChangeThemeColor = (colorCode)=>{
+    // use can also use user input value
+    setThemeColor(colorCode)
+    document.querySelector(':root').style.setProperty('--primary', colorCode);
+
+  }
+
+  return (
+    <div className={`w-full text-center text-primary m-4`}>
+      <h1>Theme Practice</h1>
+      <div className="m-4 cursor-pointer" onClick={() => handleDarkModeSelection()}>{!isDarModeActive ? "Enable Dark Mode" : "Enable Light Mode"}</div>
+      <div>
+        <div>Change Theme Color</div>
+        <div>
+          <div className="m-4 cursor-pointer" onClick={()=>handleChangeThemeColor("red")}>Red</div>
+          <div className="m-4 cursor-pointer" onClick={()=>handleChangeThemeColor("green")}>green</div>
+          <div className="m-4 cursor-pointer" onClick={()=>handleChangeThemeColor("yellow")}>yellow</div>
+          <div className="m-4 cursor-pointer" onClick={()=>handleChangeThemeColor("orange")}>orange</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
