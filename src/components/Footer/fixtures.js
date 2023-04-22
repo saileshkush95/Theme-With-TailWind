dc7f324ff1aeddda3fe0a8c70e87e020eb579605
Sandeep Kushwaha <saileshkush95@gmail.com>
2023-04-22 20:02:32 +0545

    ..

---
+++ src/components/Footer/fixtures.js
@@ -0,0 +1,44 @@
+const navigationMenu = [
+  {
+    name: "Dashboard",
+    path:"/",
+    icon: "dashboard.png",
+    activeIcon: "dashboard.png",
+    open: false,
+    children:[]
+  },
+  {
+    name: "Dashboard",
+    path:"/",
+    icon: "dashboard.png",
+    activeIcon: "dashboard.png",
+    open: false,
+    children:[]
+  },
+  {
+    name: "Dashboard",
+    path:"/",
+    icon: "dashboard.png",
+    activeIcon: "dashboard.png",
+    open: false,
+    children:[]
+  },
+  {
+    name: "Dashboard",
+    path:"/",
+    icon: "dashboard.png",
+    activeIcon: "dashboard.png",
+    open: false,
+    children:[]
+  },
+  {
+    name: "Dashboard",
+    path:"/",
+    icon: "dashboard.png",
+    activeIcon: "dashboard.png",
+    open: false,
+    children:[]
+  },
+  
+]
+export default navigationMenu
